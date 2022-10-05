80c382a2a1973d7f9b6a61559a8ae381523c874c
dvonnew <dvonnewilliams1@gmail.com>
2022-10-05 16:08:57 -0400

    some updates

--- src/Components/ContactComponents/Contact.js
+++ src/Components/ContactComponents/Contact.js
@@ -12,7 +12,14 @@ const Contact = (props) => {
                 <p>A: 1150 Galapago St, APT 909 Denver CO, 80204</p>
                 <p>E: dvonnewilliams1@gmail.com</p>
                 <p>P: 978-844-8682</p>
-                <div className="my-links"></div>
+                <div className="my-links">
+                    <a href='https://github.com/dvonnew' className='links' target="_blank">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original.svg" alt="github"/>
+                    </a>
+                    <a href='https://www.linkedin.com/in/dvonne-williams/' className='links' target="_blank">
+                        <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/linkedin/linkedin-plain.svg" />
+                    </a>
+                </div>
             </div>
             <div className="contact-photo">
                 <img src={image} alt="contact" />
